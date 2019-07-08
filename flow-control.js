64be84804e97e588e67f08fc253899a0175a7e2b
Mike McTighe <m.mctighe10@gmail.com>
2019-07-08 23:20:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -8,12 +8,22 @@ return ("You are a teenager!")
 
 function teenager(age) {
   if (age<= 19 && age >= 13){
-    return ("You ")
+    return ("You are a teenager ")
   } else {return ("your not a teenager")
 }
 }
 
-function ageChecker(age) {
+function ageChecker(age) { 
+  if (age <19 && age >= 13) {
+    return ("your a teenager")
+  } if-else{ (age < 12)
+    return ("your a kid")
+  } else {("age > 19")
+    return ("your an adult")
+}
+  }
+    
+  }
 
 }
 
